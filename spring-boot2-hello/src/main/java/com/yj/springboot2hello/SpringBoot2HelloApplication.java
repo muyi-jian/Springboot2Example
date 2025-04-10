d0a4c0a06d5fb7b1891c9fa1b13d02546cd852ec
muyi-jian <2628168756@qq.com>
2025-04-10 19:57:07 +0800

    提交hello模块代码

---
+++ spring-boot2-hello/src/main/java/com/yj/springboot2hello/SpringBoot2HelloApplication.java
@@ -0,0 +1,13 @@
+package com.yj.springboot2hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2HelloApplication.class, args);
+	}
+
+}
